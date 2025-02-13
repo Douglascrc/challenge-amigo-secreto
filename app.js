4037c5a082d3fb79adf3d622094c1c1734755c64
Douglascrc <douglasbuscam@gmail.com>
2025-02-12 22:44:39 -0300

    Implementa toda a lógica de adicionar e sortear amigos

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// 1) Quais são os dados de entrada do programa? amigos a serem sorteados
+// 2) O que deve ser feito com esses dados? sortear um amigo para cada vez que o botão for clicado
+// 3) Quais as restrições do programa? não pode sortear o mesmo amigo duas vezes
+// 4) Quais a saída esperada? amigo sorteado
+// 5) Qual a sequência de passos para chegar a saída esperada? armazenar os amigos em um array, sortear os amigos e imprimir o amigo sorteado
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigoValor = document.getElementById('amigo').value;
+
+    // Verifica se o input não está vázio
+    if(amigoValor === '') {
+        window.alert('Por favor, insira um nome!');
+    }
+    // Verifica se o array `amigos` já não contém o nome.
+    else if(amigos.includes(amigoValor)) {
+      window.alert('O amigo já consta na lista');
+      return;  
+    }
+
+    // Adiciona um novo amigo a ser sorteado.
+    amigos.push(amigoValor);
+    document.getElementById('amigo').value = '';
+    document.getElementById('listaAmigos').innerHTML = amigos;
+    console.log(amigos);
+
+}
+
+function sortearAmigo() {
+    // Verifica se tem algum amigo para sortear.
+    if(amigos.length === 0) {
+        window.alert('Não há amigos para sortear, digite o nome deles abaixo!');
+        return;
+    }
+    // Efetivamente sorteia
+    let indexSorteado = Math.floor(Math.random * amigos.length);
+
+    // Remove o index do amigo sorteado para que ele não seja sorteado novamente
+    let amigoSorteado = amigos.splice(indexSorteado,1)[0];
+    document.getElementById('resultado').innerHTML = `O Amigo sorteado foi ${amigoSorteado}`;
+}
